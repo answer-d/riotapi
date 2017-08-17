74f051e0e318c39ffd45d532ced06a1e62a1a18b
answer_d <bboy.answer.d@gmail.com>
2017-08-17 15:51:32 +0900

    loggerついか

--- script/api_caller.rb
+++ script/api_caller.rb
@@ -26,8 +26,8 @@ class APICaller
   # arg : SN
   # ret : サモナー情報(summonerid, accountid, summonername等)
   def self.summoner_byname(name)
-    @@logger.debug("#{@@basename} : summoner_byname(#{name}) start")
-
+    @@logger.info("#{@@basename} : summoner_byname(#{name}) start")
+    
     if name.empty?
       e = RiotAPIException.new(0, "サモナーネームに何か入れて下さいよホンマ")
       @@logger.info("#{@@basename} : summoner name is empty. raise #{e}")
@@ -36,13 +36,13 @@ class APICaller
     
     uri_api = "/lol/summoner/v3/summoners/by-name/#{name}"
     @@logger.debug("#{@@basename} : uri_api : #{uri_api}")
-
+    
     begin
       @@logger.debug("#{@@basename} : call get_json(#{@@uri_head + uri_api + @@uri_foot})")
       json = get_json(@@uri_head + uri_api + @@uri_foot)
       @@logger.debug("#{@@basename} : ret get_json(#{@@uri_head + uri_api + @@uri_foot}) => #{json}")
     rescue RiotAPIException => e
-      @@logger.warn("#{@@basename} : RiotAPIException occured : #{e}")
+      @@logger.warn("#{@@basename} : #{e} occured")
       case e.code
       when 404
         @@logger.info("#{@@basename} : 404 not found(SN : #{name})")
@@ -54,67 +54,92 @@ class APICaller
       raise e
     end
     
-    @@logger.debug("#{@@basename} : summoner_byname(#{name}) end => #{json}")
+    @@logger.info("#{@@basename} : summoner_byname(#{name}) end => #{json}")
     return json
   end
 
   # arg : SID
   # ret : 進行中ゲーム情報
   def self.activegame_byid(id)
+    @@logger.info("#{@@basename} : activegame_byid(#{id}) start")
     uri_api = "/lol/spectator/v3/active-games/by-summoner/#{id}"
     
     begin
+      @@logger.debug("#{@@basename} : call get_json(#{@@uri_head + uri_api + @@uri_foot})")
       json = get_json(@@uri_head + uri_api + @@uri_foot)
+      @@logger.debug("#{@@basename} : ret get_json(#{@@uri_head + uri_api + @@uri_foot}) => #{json}")
     rescue RiotAPIException => e
+      @@logger.warn("#{@@basename} : #{e} occured")
       case e.code
       when 404
+        @@logger.info("#{@@basename} : 404 not found(SN : #{id})")
         e.msg += "<br>\nゲーム中じゃ無いと思います、確認して再実行オナシャス(SID : #{id})"
       end
+      @@logger.warn("#{@@basename} : propagates #{e}")
       raise e
     end
-
+    
+    @@logger.info("#{@@basename} : activegame_byid(#{id}) end => #{json}")
     return json
   end
   
   # arg : SID
   # ret : ランク(Tier, Division等)
   def self.position_byid(id)
+    @@logger.info("#{@@basename} : position_byid(#{id}) start")
     uri_api = "/lol/league/v3/positions/by-summoner/#{id}"
     
     begin
+      @@logger.debug("#{@@basename} : call get_json(#{@@uri_head + uri_api + @@uri_foot})")
       json = get_json(@@uri_head + uri_api + @@uri_foot).at(0)
+      @@logger.debug("#{@@basename} : ret get_json(#{@@uri_head + uri_api + @@uri_foot}) => #{json}")
     rescue RiotAPIException => e
+      @@logger.warn("#{@@basename} : #{e} occured")
+      @@logger.warn("#{@@basename} : propagates #{e}")
       raise e
     end
-
+    
+    @@logger.info("#{@@basename} : position_byid(#{id}) end => #{json}")
     return json
   end
 
   # arg : なし
   # ret : マスタリー情報(static)
   def self.mastery_static()
+    @@logger.info("#{@@basename} : mastery_static() start")
     uri_api = "/lol/static-data/v3/masteries"
     
     begin
+      @@logger.debug("#{@@basename} : call get_json(#{@@uri_head + uri_api + @@uri_foot + '&locale=ja_JP'})")
       json = get_json(@@uri_head + uri_api + @@uri_foot + '&locale=ja_JP')
+      @@logger.debug("#{@@basename} : ret get_json(#{@@uri_head + uri_api + @@uri_foot + '&locale=ja_JP'}) => #{json}")
     rescue RiotAPIException => e
+      @@logger.warn("#{@@basename} : #{e} occured")
+      @@logger.warn("#{@@basename} : propagates #{e}")
       raise e
     end
-
+    
+    @@logger.info("#{@@basename} : mastery_static() end => #{json}")
     return json
   end
   
   # arg : なし
   # ret : チャンピオン情報(static)
   def self.champion_static()
+    @@logger.info("#{@@basename} : champion_static() start")
     uri_api = "/lol/static-data/v3/champions"
     
     begin
+      @@logger.debug("#{@@basename} : call get_json(#{@@uri_head + uri_api + @@uri_foot + '&locale=ja_JP'})")
       json = get_json(@@uri_head + uri_api + @@uri_foot + '&locale=ja_JP')
+      @@logger.debug("#{@@basename} : ret get_json(#{@@uri_head + uri_api + @@uri_foot + '&locale=ja_JP'}) => #{json}")
     rescue RiotAPIException => e
+      @@logger.warn("#{@@basename} : #{e} occured")
+      @@logger.warn("#{@@basename} : propagates #{e}")
       raise e
     end
-
+    
+    @@logger.info("#{@@basename} : champion_static() end => #{json}")
     return json
   end
   
@@ -124,12 +149,14 @@ class APICaller
   # URIからJSONをパースして返す感じ
   # 上の各メソッドから呼んで使う共通処理的なやつ
   def self.get_json(uri)
+    @@logger.info("#{@@basename} : get_json(#{uri}) start")
     uri = URI.parse URI.encode(uri)
     res = Net::HTTP.get_response(uri)
     
     if res.code != '200'
+      @@logger.warn("#{@@basename} : res.code is #{res.code}")
       code = res.code.to_i
-      msg = "APIコールでエラー : #{uri}(#{code})"
+      msg = "APIコールでエラー : #{code}"
       
       case code
       when 400
@@ -139,14 +166,18 @@ class APICaller
       when 429
         msg += "<br>\nれーとりみっとです、加減してくださいお願いします何でもしますから(何でもするとは言っていない)"
       when 500
-        msg += "<br>\nRiotAPI側のエラー。Rito plz"
+        msg += "<br>\nRiotAPI側のエラー、Rito plz"
       end
       
-      raise RiotAPIException.new(code, msg)
+      e = RiotAPIException.new(code, msg)
+      @@logger.warn("#{@@basename} : raise #{e}")
+      raise e
     end
+    @@logger.debug("#{@@basename} : res.code is 200")
     
     json = JSON.load(res.body)
+    
+    @@logger.info("#{@@basename} : get_json(#{uri}) end => #{json}")
     return json
   end
 end
-
--- script/html_generator.rb
+++ script/html_generator.rb
@@ -7,8 +7,18 @@ require 'csv'
 require_relative 'api_caller.rb'
 
 class HtmlGenerator
+  # 固定値軍団
+  @@basedir = File.expand_path(File.dirname(__FILE__))
+  @@basename = File.basename($0, ".rb")
+  @@conf = YAML.load_file("#{@@basedir}/../conf/app.yml")
+  
+  @@logger = Logger.new("#{@@basedir}/../log/#{File.basename($0, "rb")}.log")
+  @@logger.level = eval @@conf["logger"]["log_level"]
+  
   # 現在のランクを表示
   def self.cur_rank(summoner_id)
+    @@logger.info("#{@@basename} : cur_rank(#{summoner_id}) start")
+    
     refresh_rate = '30' #秒
     window_width = '256' #px
     window_height = '64' #px
@@ -16,8 +26,12 @@ class HtmlGenerator
     font_options = 'size="5" color="white"'
 
     begin
+      @@logger.debug("#{@@basename} : call APICaller.position_byid(#{summoner_id})")
       hash = APICaller.position_byid(summoner_id)
+      @@logger.debug("#{@@basename} : ret APICaller.position_byid(#{summoner_id}) => #{hash}")
     rescue RiotAPIException => e
+      @@logger.warn("#{@@basename} : #{e} occured")
+      @@logger.warn("#{@@basename} : propagates #{e}")
       raise e
     end
     
@@ -41,10 +55,13 @@ class HtmlGenerator
       </html>
     EOS
     
+    @@logger.info("#{@@basename} : cur_rank(#{summoner_id}) end => buf")
     return buf
   end
   
   def self.cur_keystones(name)
+    @@logger.info("#{@@basename} : cur_keystones(#{name}) start")
+    
     window_width=1920 #全体の横幅
     icon_width=(window_width*0.05).floor+1 #チャンピオン情報の横幅
     icon_height=111 #チャンピオン情報の縦幅(チャンピオン毎)
@@ -60,19 +77,29 @@ class HtmlGenerator
     mastery_list = CSV.read(File.expand_path(File.dirname(__FILE__)) + '/../data/mastery.csv') #配列の配列
     mastery_hash = mastery_list.inject({}){|h, elem| h[elem[0]] = elem[1]; h} #ハッシュ化
     keystone_masteries = keystone_names.inject({}){|h, elem| h[mastery_hash.key(elem)] = elem; h} #キーストーンだけ抜き出し
+    @@logger.debug("keystone_masteries : #{keystone_masteries}")
 
     # サモナーネームからサモナーIDを引っ張る
     begin
+      @@logger.debug("#{@@basename} : call APICaller.summoner_byname(#{name})")
       summoner_json = APICaller.summoner_byname(name)
+      @@logger.debug("#{@@basename} : ret APICaller.summoner_byname(#{name}) => #{summoner_json}")
     rescue RiotAPIException => e
+      @@logger.warn("#{@@basename} : #{e} occured")
+      @@logger.warn("#{@@basename} : propagates #{e}")
       raise e
     end
     summoner_id = summoner_json["id"]
+    @@logger.info("#{@@basename} : #{name}'s summoner_id is #{summoner_id}")
     
     # サモナーIDから進行中ゲーム情報のjsonを引っ張る
     begin
+      @@logger.debug("#{@@basename} : call APICaller.activegame_byid(#{summoner_id})")
       json = APICaller.activegame_byid(summoner_id)
+      @@logger.debug("#{@@basename} : ret APICaller.activegame_byid(#{summoner_id}) => #{json}")
     rescue RiotAPIException => e
+      @@logger.warn("#{@@basename} : #{e} occured")
+      @@logger.warn("#{@@basename} : propagates #{e}")
       raise e
     end
     
@@ -98,81 +125,96 @@ class HtmlGenerator
     
     img_options = %!width="#{ins_icon_sides}" height="#{ins_icon_sides}"!
     [100,200].each{|teamId|
+      @@logger.debug("#{@@basename} : loop for teamId=#{teamId} start")
       buf += %!<td><table border=0 width="#{icon_width}" height="#{icon_height*5}" cellspacing="0" cellpadding="0">!
       json["participants"].select{|elem| elem["teamId"] == teamId}.each{|elem|
+        @@logger.debug("#{@@basename} : loop for participant=#{elem["summonerName"]} - #{elem["summonerId"]} start")
         part_masteries = elem["masteries"].map{|hash| hash["masteryId"].to_s}
         part_keystone = part_masteries.find{|i| keystone_masteries.keys.include? i}
+        @@logger.debug("#{@@basename} : part_keystone=#{partkeystone}")
         
         begin
+          @@logger.debug("#{@@basename} : call APICaller.position_byid(#{elem["summonerId"]})")
           l_json = APICaller.position_byid(elem["summonerId"])
+          @@logger.debug("#{@@basename} : ret APICaller.position_byid(#{elem["summonerId"]}) => #{l_json}")
         rescue RiotAPIException => e
+          @@logger.warn("#{@@basename} : #{e} occured")
           e.msg += "<br>\nランク情報を引くのに失敗したンゴ…(#{elem["summonerName"]} - #{elem["summonerId"]})"
+          @@logger.warn("#{@@basename} : propagates #{e}")
+          raise e
         end
 
         img_rank = %!../img/#{l_json.nil? ? "UNRANK" : l_json["tier"]+l_json["rank"]}.png!
         img_keystone = %!../img/#{part_keystone}.png!
+        @@logger.debug("#{@@basename} : img_rank=#{img_rank}")
+        @@logger.debug("#{@@basename} : img_keystone=#{img_keystone}")
 
         buf += <<-EOS
-        <!--
-        #{l_json}
-        -->
-        <tr height="#{ins_t_margin}">
-        <td width="#{teamId == 100 ? ins_l_margin : ins_icon_sides}"></td>
-        <td width="#{teamId == 100 ? ins_icon_sides : icon_width - ins_l_margin - ins_icon_sides*2}"></td>
-        <td width="#{teamId == 100 ? icon_width - ins_l_margin - ins_icon_sides*2 : ins_icon_sides}"></td>
-        <td width="#{teamId == 100 ? ins_icon_sides : ins_l_margin}"></td>
-        </tr>
-        <tr height="#{ins_icon_sides}">
-        <td width="#{teamId == 100 ? ins_l_margin : ins_icon_sides}"></td>
-        <td width="#{teamId == 100 ? ins_icon_sides : icon_width - ins_l_margin - ins_icon_sides*2}">
-      EOS
-      buf += %!<img src="#{img_rank}" #{img_options}>! if teamId == 100
-      buf += <<-EOS
-        </td>
-        <td width="#{teamId == 100 ? icon_width - ins_l_margin - ins_icon_sides*2 : ins_icon_sides}">
-      EOS
-      buf += %!<img src=#{img_rank} #{img_options}>! if teamId == 200
-      buf += <<-EOS
-        </td><td width="#{teamId == 100 ? ins_icon_sides : ins_l_margin}"></td>
-        </tr>
-        <tr height="#{ins_v_margin}">
-        <td width="#{teamId == 100 ? ins_l_margin : ins_icon_sides}"></td>
-        <td width="#{teamId == 100 ? ins_icon_sides : icon_width - ins_l_margin - ins_icon_sides*2}"></td>
-        <td width="#{teamId == 100 ? icon_width - ins_l_margin - ins_icon_sides*2 : ins_icon_sides}"></td>
-        <td width="#{teamId == 100 ? ins_icon_sides : ins_l_margin}"></td>
-        </tr>
-        <tr height="#{ins_icon_sides}">
-        <td width="#{teamId == 100 ? ins_l_margin : ins_icon_sides}">
-      EOS
-      buf += %!<img src="#{img_keystone}" #{img_options}>! if teamId == 200 && !part_keystone.nil?
-      buf += <<-EOS
-        </td><td width="#{teamId == 100 ? ins_icon_sides : icon_width - ins_l_margin - ins_icon_sides*2}"></td>
-        <td width="#{teamId == 100 ? icon_width - ins_l_margin - ins_icon_sides*2 : ins_icon_sides}"></td>
-        <td width="#{teamId == 100 ? ins_icon_sides : ins_l_margin}">
-      EOS
-      buf += %!<img src="#{img_keystone}" #{img_options}>! if teamId == 100 && !part_keystone.nil?
-      buf += <<-EOS
-        </td></tr>
-        <tr height="#{icon_height - ins_t_margin - ins_v_margin - ins_icon_sides*2}">
-        <td width="#{teamId == 100 ? ins_l_margin : ins_icon_sides}"></td>
-        <td width="#{teamId == 100 ? ins_icon_sides : icon_width - ins_l_margin - ins_icon_sides*2}"></td>
-        <td width="#{teamId == 100 ? icon_width - ins_l_margin - ins_icon_sides*2 : ins_icon_sides}"></td>
-        <td width="#{teamId == 100 ? ins_icon_sides : ins_l_margin}"></td>
-        </tr>
-      EOS
-    }
-    buf += %!</table></td>!
-    buf += %!<td></td>! if teamId == 100
-    sleep 0.5 #API制限緩和用
+          <!--
+          #{l_json}
+          -->
+          <tr height="#{ins_t_margin}">
+          <td width="#{teamId == 100 ? ins_l_margin : ins_icon_sides}"></td>
+          <td width="#{teamId == 100 ? ins_icon_sides : icon_width - ins_l_margin - ins_icon_sides*2}"></td>
+          <td width="#{teamId == 100 ? icon_width - ins_l_margin - ins_icon_sides*2 : ins_icon_sides}"></td>
+          <td width="#{teamId == 100 ? ins_icon_sides : ins_l_margin}"></td>
+          </tr>
+          <tr height="#{ins_icon_sides}">
+          <td width="#{teamId == 100 ? ins_l_margin : ins_icon_sides}"></td>
+          <td width="#{teamId == 100 ? ins_icon_sides : icon_width - ins_l_margin - ins_icon_sides*2}">
+        EOS
+        buf += %!<img src="#{img_rank}" #{img_options}>! if teamId == 100
+        buf += <<-EOS
+          </td>
+          <td width="#{teamId == 100 ? icon_width - ins_l_margin - ins_icon_sides*2 : ins_icon_sides}">
+        EOS
+        buf += %!<img src=#{img_rank} #{img_options}>! if teamId == 200
+        buf += <<-EOS
+          </td><td width="#{teamId == 100 ? ins_icon_sides : ins_l_margin}"></td>
+          </tr>
+          <tr height="#{ins_v_margin}">
+          <td width="#{teamId == 100 ? ins_l_margin : ins_icon_sides}"></td>
+          <td width="#{teamId == 100 ? ins_icon_sides : icon_width - ins_l_margin - ins_icon_sides*2}"></td>
+          <td width="#{teamId == 100 ? icon_width - ins_l_margin - ins_icon_sides*2 : ins_icon_sides}"></td>
+          <td width="#{teamId == 100 ? ins_icon_sides : ins_l_margin}"></td>
+          </tr>
+          <tr height="#{ins_icon_sides}">
+          <td width="#{teamId == 100 ? ins_l_margin : ins_icon_sides}">
+        EOS
+        buf += %!<img src="#{img_keystone}" #{img_options}>! if teamId == 200 && !part_keystone.nil?
+        buf += <<-EOS
+          </td><td width="#{teamId == 100 ? ins_icon_sides : icon_width - ins_l_margin - ins_icon_sides*2}"></td>
+          <td width="#{teamId == 100 ? icon_width - ins_l_margin - ins_icon_sides*2 : ins_icon_sides}"></td>
+          <td width="#{teamId == 100 ? ins_icon_sides : ins_l_margin}">
+        EOS
+        buf += %!<img src="#{img_keystone}" #{img_options}>! if teamId == 100 && !part_keystone.nil?
+        buf += <<-EOS
+          </td></tr>
+          <tr height="#{icon_height - ins_t_margin - ins_v_margin - ins_icon_sides*2}">
+          <td width="#{teamId == 100 ? ins_l_margin : ins_icon_sides}"></td>
+          <td width="#{teamId == 100 ? ins_icon_sides : icon_width - ins_l_margin - ins_icon_sides*2}"></td>
+          <td width="#{teamId == 100 ? icon_width - ins_l_margin - ins_icon_sides*2 : ins_icon_sides}"></td>
+          <td width="#{teamId == 100 ? ins_icon_sides : ins_l_margin}"></td>
+          </tr>
+        EOS
+        @@logger.debug("#{@@basename} : loop for participant=#{elem["summonerName"]} - #{elem["summonerId"]} end")
+      }
+      buf += %!</table></td>!
+      buf += %!<td></td>! if teamId == 100
+      sleep 0.5 #API制限緩和用
+      @@logger.debug("#{@@basename} : loop for teamId=#{teamId} end")
     }
     buf += %!</tr></table></body></html>!
     
     # ファイル書き出し
     # overlayフォルダはapacheユーザに書き込み権限があること
-    File.open(File.expand_path(File.dirname(__FILE__)).gsub("script", "html/overlay/") + summoner_id.to_s + '.html', "w"){|f|
+    @@logger.info("#{@@basename} : create #{@@basedir.gsub("script", "html/overlay/") + summoner_id.to_s + '.html'}")
+    @@logger.debug("#{buf}")
+    File.open(@@basedir.gsub("script", "html/overlay/") + summoner_id.to_s + '.html', "w"){|f|
       f.puts buf
     }
-
+    
+    @@logger.info("#{@@basename} : cur_keystones(#{name}) end => #{summoner_id}")
     return summoner_id
   end
 end
